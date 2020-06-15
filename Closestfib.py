b217e732811c0cafd5a3ad96c9a4ee7b130d3e95
Nestor Caceres <necaro55@gmail.com>
2020-06-15 17:26:52 -0600

    Create Closestfib.py
    Gets the closest Fibonacci numbers above and below given N.

---
+++ Closestfib.py
@@ -0,0 +1,7 @@
+import math
+def closestFib(n):
+    golden = 0.5 + ((5**0.5) / 2)
+    nearestPlace = round(math.log(n*(5**0.5),golden))
+    x1 = round(((golden**(nearestPlace)) - ((1 - golden)**(nearestPlace)))/(5**0.5))
+    x2 = round(x1 * golden)
+    return (x1,x2)
